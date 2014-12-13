65befad2c321e72f9cdd4511366c21b6f1147086
Robert Gwynn-Jones <gwynnr@gmail.com>
2014-12-14 00:14:56 +1100

    Move default OAuth2 keys from base settings to local

--- settings/base.py
+++ settings/base.py
@@ -54,9 +54,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '331367153417-dkca7e4oenfpe22enn13v3po0esn24tn.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'jcKGcrItSq_6boq972_OODcG'
-
 TEMPLATE_CONTEXT_PROCESSORS = (
     'social.apps.django_app.context_processors.backends',
     'social.apps.django_app.context_processors.login_redirect',
--- settings/local.py
+++ settings/local.py
@@ -1,3 +1,6 @@
 """Development settings and globals."""
 
 from .base import *
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '331367153417-dkca7e4oenfpe22enn13v3po0esn24tn.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'jcKGcrItSq_6boq972_OODcG'

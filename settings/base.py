22978400d72bfd1a3f959e6b490450278161c195
Robert Gwynn-Jones <gwynnr@gmail.com>
2014-12-14 12:25:14 +1100

    Move check for local secrets.json file to local settings from base settings

--- settings/base.py
+++ settings/base.py
@@ -10,24 +10,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import json
-
-from django.core.exceptions import ImproperlyConfigured
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-SECRETS_LOC = os.path.join(BASE_DIR, 'settings', 'secrets.json')
-
-with open(SECRETS_LOC) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    """Get the secret variable of return an explicit exception"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {0} property in secrets.json".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
